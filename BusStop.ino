1d1a7bdf48e04257efe111ad25836be04cce5975
Tom Stewart <gitstuff@tastewar.com>
2014-02-17 16:06:01 -0500

    Separating out data gathering from data display

--- BusStop.ino
+++ BusStop.ino
@@ -25,6 +25,11 @@
 // the priority file will be cancelled and the alert will remain as an ordinary message in
 // sequence.
 //
+// Since alerts are unpredictable, and there can be any number active at a given time, we will
+// allocate memory for them on the fly, and track them in two linked lists: active, and available.
+// When a new alert comes in, we will first look for an available buffer, but if not found, we
+// will allocate a new one. Buffers move to the available list when the alert expires.
+//
 // sign config
 //
 */
@@ -34,6 +39,7 @@
 #include <RTClib.h>
 #include <TinyXML.h>
 #include <DigiFi.h>
+#include <LinkedList.h>
 
 #define SECONDS_FROM_1970_TO_2000 946684800
 #define StopNumber "2282"
@@ -51,12 +57,12 @@
 #define MBTAAlertsByStopURL MBTARootURL "alertsbystop" MBTAAPIKey "&stop=" StopNumber
 #define MaxNextBusPredictions 5
 #define MaxPriorityMessageLength 125
+#define MaxAlertMessageLength 230
 #define MilliSecondsBetweenChecks 10000
-#define MaxAlerts 5
 #define buflen 150
 
 
-unsigned long LastCheckTime, MBTAEpochTime;
+unsigned long LastCheckTime, MBTAEpochTime, TimeTimeStamp;
 unsigned char numRoutes=0;
 TinyXML xml;
 uint8_t boofer[buflen];
@@ -64,15 +70,15 @@ uint8_t boofer[buflen];
 typedef struct _Pred
 {
 	boolean	layover;
-	char	mins[4]; // max='999\0'
+	long	mins;
 } Pred;
 
 typedef struct _RoutePred
 {
 	// NextBus provides up to 5
 	boolean  displayed;
-	char     routeid[12];
-        char     routename[12];
+	char     routeid[12]; // internal use, alpha-numeric uses no punctuation
+        char     routename[12]; // for display
 	Pred     pred[MaxNextBusPredictions];
 } RoutePred;
 
@@ -81,11 +87,13 @@ typedef struct _AlertMsg
   boolean        alerted;
   unsigned long  MessageID;
   unsigned long  Expiration;
-  char           Message[MaxPriorityMessageLength+1]; //max 125 characters plus terminating null
+  char           Message[MaxAlertMessageLength+1]; //max 125 characters plus terminating null
 } AlertMsg;
 
-AlertMsg	Alerts[MaxAlerts];
-RoutePred	*Routes;
+LinkedList<AlertMsg*> ActiveAlertList = LinkedList<AlertMsg*>();
+LinkedList<AlertMsg*> FreeAlertList = LinkedList<AlertMsg*>();
+LinkedList<RoutePred*> RouteList = LinkedList<RoutePred*>();
+
 DigiFi		wifi;
 
 void setup ( )
@@ -105,9 +113,7 @@ void setup ( )
 	}
 
 	Serial.println("Connected to wifi!");
-//	xml.init((uint8_t*)&buffer,buflen,&XML_callback);
 	MBTACountRoutesByStop ( );
-	Routes = (RoutePred *)malloc(numRoutes * sizeof(RoutePred));
 	ConfigureDisplay ( );
 	LastCheckTime=millis()-MilliSecondsBetweenChecks;
         Serial.println ( "Done with setup." );
@@ -120,7 +126,7 @@ void loop ( )
    MaybeCheckForNewData ( );
 }
 
-unsigned char MBTACountRoutesByStop ( )
+void MBTACountRoutesByStop ( )
 {
   Serial.println ( "Getting list of routes for the stop." );
   GetXML ( MBTAServer, MBTARoutesByStopURL, RouteListXMLCB );
@@ -128,6 +134,10 @@ unsigned char MBTACountRoutesByStop ( )
 
 void ConfigureDisplay ( )
 {
+  // configure 1 file to display the time with the text "MBTA Time: " and a ref to a date string file, space, ref to a time string file + any formatting
+  // configure n text files, 1 for each route with 12 characters for route number, a colon, a space, any formatting, and a reference to a string file
+  // configure n string files, 1 for the predictions of each route. 5 predictions, and for each prediction maybe a tilde, 3 digits, a comma, and a space
+  // divide the remaining files in half, with half being text files that say "Alert: " plus any formatting; the other half as string files 230 bytes long
 }
 
 void DHCPandStatusCheck ( )
@@ -136,6 +146,7 @@ void DHCPandStatusCheck ( )
 
 void MaybeUpdateDisplay ( )
 {
+  // for BetaBrite, we'll have to update the run sequence when the list of active alerts changes
 }
 
 void MaybeCheckForNewData ( )
@@ -157,15 +168,12 @@ void GetXML ( char *ServerName, char *Page, XMLcallback fcb )
 	if ( wifi.connect ( ServerName, 80 ) == 1 )
 	{
                 unsigned long tim;
-                //Serial.println ( "Connected." );
-		wifi.print("GET "); //Serial.print ("GET ");
-		wifi.print(Page); //Serial.print ( Page );
-		wifi.print(" HTTP/1.1\r\nHost: "); //Serial.print(" HTTP/1.1\r\nHost: ");
-		wifi.println(ServerName); //Serial.println(ServerName);
-                wifi.println("Connection: close"); //Serial.println ( "Connection: close" );
-		wifi.println("Accept: application/xml\r\n"); // should make 2nd crlf for empty line to terminate header
-                //Serial.println("Accept: application/xml\r\n");
-                //Serial.println ("<end of request>");
+		wifi.print("GET ");
+		wifi.print(Page);
+		wifi.print(" HTTP/1.1\r\nHost: ");
+		wifi.println(ServerName);
+                wifi.println("Connection: close");
+		wifi.println("Accept: application/xml\r\n");
                 tim  = millis ( );
 		while (true)
 		{
@@ -177,11 +185,9 @@ void GetXML ( char *ServerName, char *Page, XMLcallback fcb )
 			// we simply drop any characters before that
 			if (c=='<')
 			{
-                                //Serial.println ( "Got to the XML payload");
 				xml.processChar('<');
 				break;
 			}
-                        //else Serial.print ( c );
 		  }
                   else if ( millis ( ) - tim > 10000 )
                   {
@@ -228,7 +234,7 @@ void NextBusCheckPredictions ( )
   GetXML ( NextBusServer, NextBusPredictionURL, PredictionsXMLCB );
 }
 
-void XML_callback ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  char* data,  uint16_t dataLen )
+void XML_GenericCallback ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  char* data,  uint16_t dataLen )
 {
   if (statusflags & STATUS_START_TAG)
   {
@@ -282,22 +288,8 @@ void ServerTimeXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,
     {
       if ( strcmp ( tagName, "server_dt" ) == 0 && strcmp ( pTag, "/server_time" == 0 ) )
       {
-        //Serial.print ( "tag: " );
-        //Serial.println ( pTag );
-        Serial.print ( "The time is: " );
-        Serial.println ( data );
         MBTAEpochTime = atol ( data );
-        uint32_t nowish = MBTAEpochTime - SECONDS_FROM_1970_TO_2000;
-        DateTime dt(nowish);
-        Serial.print(dt.year()); Serial.print("/"); Serial.print(dt.month()); Serial.print("/"); 
-        Serial.print(dt.day()); Serial.print(" "); 
-        Serial.print(dt.hour()); Serial.print(":"); 
-        unsigned char m=dt.minute();
-        if ( m < 10 ) Serial.print ( '0' );
-        Serial.print(m); Serial.print(":");
-        unsigned char s=dt.second();
-        if ( s < 10 ) Serial.print ( '0' );
-        Serial.print(s); Serial.println ( " GMT" );
+        TimeTimeStamp = millis ( );
       }
     }
   }
@@ -307,6 +299,8 @@ void RouteListXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen, 
 {
   static char *pTag;
   static bool BusMode=false;
+  static RoutePred  *pCR;
+  
   if (statusflags & STATUS_START_TAG)
   {
     if ( tagNameLen )
@@ -325,29 +319,30 @@ void RouteListXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen, 
       }
       else if ( strcmp ( tagName, "route_id" ) == 0 && strcmp ( pTag, "/route_list/mode/route" == 0 ) && BusMode )
       {
-        //Serial.print ( "route_id: " );
-        //Serial.println ( data );
         numRoutes++;
+        pCR = new RoutePred;
+        RouteList.add ( pCR );
+        strlcpy ( pCR->routeid, data, sizeof pCR->routeid );
       }
-      else if ( strcmp ( tagName, "route_name" ) == 0 && strcmp ( pTag, "/route_list/mode/route" == 0 ) && BusMode )
+      else if ( pCR && strcmp ( tagName, "route_name" ) == 0 && strcmp ( pTag, "/route_list/mode/route" == 0 ) && BusMode )
       {
-        if ( numRoutes == 1 ) Serial.print ( "Routes for this stop: " );
-        else Serial.print ( ", " );
-        Serial.print ( data );
+        strlcpy ( pCR->routename, data, sizeof pCR->routename );
       }
     }
   }
-  else if (statusflags & STATUS_END_TAG)
+/*  else if (statusflags & STATUS_END_TAG)
   {
     if ( strcmp ( tagName, "/route_list" ) == 0 && numRoutes > 0 ) Serial.println ( "" );
-  }
+  }*/
 }
 
 void AlertsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  char* data,  uint16_t dataLen )
 {
-  static char *pTag, Message[MaxPriorityMessageLength+1];
-  static bool PastStartTime=false;
+  static char *pTag;
+  static bool PastStartTime;
   static unsigned long aid;
+  static AlertMsg *pCurrentAlert;
+  
   if (statusflags & STATUS_START_TAG)
   {
     if ( tagNameLen )
@@ -357,9 +352,9 @@ void AlertsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  ch
   }
   else if  (statusflags & STATUS_TAG_TEXT)
   {
-    if ( strcmp ( tagName, "/alerts/alert/header_text" ) == 0 )
+    if ( strcmp ( tagName, "/alerts/alert/header_text" ) == 0 && pCurrentAlert )
     {
-      strlcpy ( Message, data, sizeof Message );
+      strlcpy ( pCurrentAlert->Message, data, sizeof pCurrentAlert->Message );
     }
   }
   else if  (statusflags & STATUS_ATTR_TEXT)
@@ -368,22 +363,51 @@ void AlertsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  ch
     {
       if ( strcmp ( tagName, "alert_id" ) == 0 && strcmp ( pTag, "/alerts/alert" ) == 0 )
       {
-        Serial.print ( "alert_id: " ); Serial.println ( data );
         aid = atol ( data );
+        int a, s=ActiveAlertList.size ( );
+        AlertMsg  *pA;
+        pCurrentAlert = 0;
+        for ( a=0; a<s; a++ )
+        {
+          pA = ActiveAlertList.get ( a );
+          if ( pA->MessageID == aid )
+          {
+            pCurrentAlert = pA;
+            break;
+          }
+        }
+        if ( !pCurrentAlert ) // get a new one or allocate
+        {
+          if ( FreeAlertList.size ( ) > 0 )
+          {
+            pCurrentAlert = FreeAlertList.pop ( );
+          }
+          else
+          {
+            pCurrentAlert = new AlertMsg;
+          }
+          if ( pCurrentAlert )
+          {
+            ActiveAlertList.add ( pCurrentAlert );
+            pCurrentAlert->MessageID = aid;
+            pCurrentAlert->alerted = false;
+          }
+        }
       }
+      // we're only going to track a single current active period
       else if ( strcmp ( tagName, "effect_start" ) == 0 && strcmp ( pTag, "/alerts/alert/effect_periods/effect_period" ) == 0 )
       {
         if ( atol ( data ) <= MBTAEpochTime ) PastStartTime = true;
         else PastStartTime = false;
       }
-      else if ( strcmp ( tagName, "effect_end" ) == 0 && strcmp ( pTag, "/alerts/alert/effect_periods/effect_period" ) == 0 )
+      else if ( PastStartTime && strcmp ( tagName, "effect_end" ) == 0 && strcmp ( pTag, "/alerts/alert/effect_periods/effect_period" ) == 0 )
       {
-        unsigned long endTime = atol ( data );
-        if ( ( endTime > MBTAEpochTime || !data || !*data ) && PastStartTime )
+        unsigned long endTime;
+        if ( !data || !*data ) endTime = 0xFFFFFFFF;
+        else endTime = atol ( data );
+        if ( endTime > MBTAEpochTime && pCurrentAlert )
         {
-          // valid now -- add it to the array
-          Serial.println ( endTime );
-          Serial.println ( Message );
+          pCurrentAlert->Expiration = endTime;
         }
       }
     }
@@ -393,8 +417,10 @@ void AlertsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  ch
 void PredictionsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  char* data,  uint16_t dataLen )
 {
   static char *pTag;
-  static int Minutes;
-  static bool Approximate, First;
+  static int Minutes, prdno;
+  static bool Approximate;
+  static RoutePred  *pCR;
+  
   if (statusflags & STATUS_START_TAG)
   {
     if ( tagNameLen )
@@ -407,7 +433,7 @@ void PredictionsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen
       }
       else if ( strcmp ( tagName, "/body/predictions" ) == 0 )
       {
-        First = true;
+        prdno = 0;
       }
     }
   }
@@ -417,8 +443,21 @@ void PredictionsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen
     {
       if ( strcmp ( tagName, "routeTitle" ) == 0 && strcmp ( pTag, "/body/predictions" ) == 0 )
       {
-        Serial.print ( data );
-        Serial.print ( ": " );
+        int i, s;
+        RoutePred  *pR;
+        
+        s = RouteList.size ( );
+        pCR = 0;
+        for ( i=0; i<s; i++ )
+        {
+          pR = RouteList.get ( i );
+          if ( strcmp ( pR->routename, data ) == 0 )
+          {
+            pCR = pR;
+            prdno = 0;
+            break;
+          }
+        }
       }
       else if ( strcmp ( tagName, "minutes" ) == 0 && strcmp ( pTag, "/body/predictions/direction/prediction") == 0 )
       {
@@ -436,19 +475,29 @@ void PredictionsXMLCB ( uint8_t statusflags, char* tagName,  uint16_t tagNameLen
     {
       if ( Minutes != -1 )
       {
-        if ( First ) First = false;
-        else Serial.print ( ", " );
-        if ( Approximate )
-        {
-          Serial.print ( '~' );
-        }
-        Serial.print ( Minutes );
+        pCR->pred[prdno].layover = Approximate;
+        pCR->pred[prdno].mins = Minutes;
+        prdno++;
       }
     }
-    else if ( strcmp ( tagName, "/body/predictions" ) == 0 )
+/*    else if ( strcmp ( tagName, "/body/predictions" ) == 0 )
     {
       Serial.println ( "" );
-    }
+    }*/
   }
 }
 
+void DisplayTime ( void )
+{
+  uint32_t nowish = MBTAEpochTime - SECONDS_FROM_1970_TO_2000;
+  DateTime dt(nowish);
+  Serial.print(dt.year()); Serial.print("/"); Serial.print(dt.month()); Serial.print("/"); 
+  Serial.print(dt.day()); Serial.print(" "); 
+  Serial.print(dt.hour()); Serial.print(":"); 
+  unsigned char m=dt.minute();
+  if ( m < 10 ) Serial.print ( '0' );
+  Serial.print(m); Serial.print(":");
+  unsigned char s=dt.second();
+  if ( s < 10 ) Serial.print ( '0' );
+  Serial.print(s); Serial.println ( " GMT" );
+}
